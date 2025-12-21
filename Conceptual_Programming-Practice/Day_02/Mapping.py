fa756478164ae051d845eca1e17b73ecaa6ae67d
Vivek Gautam <vivekbgautam@gmail.com>
2025-12-22 01:30:14 +0530

    Add files via upload

---
+++ Conceptual_Programming-Practice/Day_02/Mapping.py
@@ -0,0 +1,11 @@
+Information = {"Name" : "Rahul", "Age" : 25, "City" : "Pune", "Marks" : 89.90}
+
+print(Information)
+print(type(Information))
+
+print(Information["City"])
+print(Information["Age"])
+print(Information["Marks"])
+print(Information["Name"])
+
+print(type(Information["Name"]))
---
+++ Conceptual_Programming-Practice/Day_02/MappingX.py
@@ -0,0 +1,11 @@
+Information = {"Name" : "Rahul", "Age" : 25, "City" : "Pune", "Marks" : 89.90, "City" : "Mumbai"}
+
+print(Information)
+
+print(Information["City"])          # Mumbai
+
+Information["Age"] = 26
+
+print(Information)
+
+print(Information["Age"])
