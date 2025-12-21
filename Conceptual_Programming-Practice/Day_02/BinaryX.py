2daa6970d9af3d2db923246b7d619aee8a3c0de4
Vivek Gautam <vivekbgautam@gmail.com>
2025-12-22 01:28:34 +0530

    Add files via upload

---
+++ Conceptual_Programming-Practice/Day_02/BinaryX.py
@@ -0,0 +1,14 @@
+#Indexed
+# Ordderd
+# Immutable
+
+Data = bytes([65,97,98])
+
+print(Data)
+print(type(Data))
+print(Data[0])
+
+# Data[0] = 66   Error Imutable
+
+print(Data)
+print(Data[0])
---
+++ Conceptual_Programming-Practice/Day_02/Datatype.py
@@ -0,0 +1,13 @@
+# Numeric
+
+Age = 35
+Marks = 89.75
+Data = 8+7j
+
+print("Ahe is : ",Age)
+print("Marks is : ",Marks)
+print("Complex : ",Data)
+
+print(type(Age))
+print(type(Marks))
+print(type(Data))
