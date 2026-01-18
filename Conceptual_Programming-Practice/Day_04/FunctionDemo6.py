5b9a9414a80060c033772850874e0518916310e5
Vivek Gautam <vivekbgautam@gmail.com>
2026-01-18 17:04:30 +0530

    function_Concept

---
+++ Conceptual_Programming-Practice/Day_04/FunctionDemo6.py
@@ -0,0 +1,10 @@
+# Accept: multiple Parameters 
+# Return: Nothing 
+def Marvellous1(Value1, Value2):
+    print("Inseide Marvellous1 : ",Value1, Value2)
+
+def main():
+    Marvellous1("Python",21)
+
+if __name__ == "__main__":
+    main()
