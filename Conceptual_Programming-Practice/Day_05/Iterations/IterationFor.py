ba83559ef17ba1c92cd2773e2c0a92e6593d50b4
Vivek Gautam <vivekbgautam@gmail.com>
2026-01-19 23:22:56 +0530

    Add files via upload

---
+++ Conceptual_Programming-Practice/Day_05/Iterations/IterationFor.py
@@ -0,0 +1,24 @@
+# /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+# //
+# //  File Name       :   IterationFor.py
+# //  Description     :   It will used to demonstrate the concept of Iteration
+# //  Author          :   Vivek Bhauraj Gautam
+# //  Date            :   10/01/2026
+# //
+# //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+# Scripting apporoach
+
+print("1")
+print("2")
+print("3")
+print("4")
+print("5")
+
+
+def main():
+    pass
+ 
+if __name__ == "__main__":
+    main()
