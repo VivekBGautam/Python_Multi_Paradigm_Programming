714157b3986dcf713613499c40934a309a0f8d33
Vivek Gautam <vivekbgautam@gmail.com>
2026-01-19 23:24:21 +0530

    Add files via upload

---
+++ Conceptual_Programming-Practice/Day_05/Iterations/IterationForXX.py
@@ -0,0 +1,36 @@
+# /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+# //
+# //  File Name       :   IterationForX.py
+# //  Description     :   It will used to demonstrate the concept of Iteration ( For LOOP )
+# //  Author          :   Vivek Bhauraj Gautam
+# //  Date            :   10/01/2026
+# //
+# //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+# Sequence apporoach
+
+print("Jay Ganesh..")
+print("Jay Ganesh..")
+print("Jay Ganesh..")
+print("Jay Ganesh..")
+
+print("-------------------------------------------------------------")
+
+# Iterartion apporoach
+
+for i in range(4):
+    print("Jay Ganesh..")
+
+# /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+# //
+# //  Function Name   :   Main  (Entry point funtion for our program)
+# //  Author          :   Vivek Bhauraj Gautam
+# //  Date            :   10/01/2026
+# //
+# //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+def main():
+    pass
+ 
+if __name__ == "__main__":
+    main()
