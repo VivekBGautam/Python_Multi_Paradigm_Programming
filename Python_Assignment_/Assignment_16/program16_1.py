6d48adfc5d0f80e0c1fd07db645f2c05f0b8f6c3
Vivek Gautam <vivekbgautam@gmail.com>
2026-01-28 23:54:35 +0530

    Add files via upload

---
+++ Python_Assignment_/Assignment_16/program16_1.py
@@ -0,0 +1,11 @@
+# Write a program which contain one function named as fun().
+# That function should display "Hello fro, fun " on console 
+
+def Fun():
+    print("Hello from Fun")
+
+def main():
+    Fun()
+
+if __name__ == "__main__":
+    main()
