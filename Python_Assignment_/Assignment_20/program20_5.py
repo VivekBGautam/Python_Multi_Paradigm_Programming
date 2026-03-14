0f6dc17a7b530396bdccdf332afb8032f184da28
Vivek Gautam <vivekbgautam@gmail.com>
2026-03-14 23:24:03 +0530

    Update program20_5.py

--- Python_Assignment_/Assignment_20/program20_5.py
+++ Python_Assignment_/Assignment_20/program20_5.py
@@ -1,4 +1,4 @@
-# 5. : Design a Python application that creates two threads named Thread1 and Thread2.  
+# 5. : Design a Python application that creates two threads named Thread1 and Thread2.   
 
 # Thread1 should display numbers from 1 to 50.
 # Thread2 should display numbers from 50 to 1 in reverse order.
