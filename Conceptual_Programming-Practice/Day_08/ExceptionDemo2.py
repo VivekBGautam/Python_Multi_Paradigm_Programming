1f87869e7b13cd6c4ba37cb686f4d9512bde772e
Vivek Gautam <vivekbgautam@gmail.com>
2026-03-17 20:08:05 +0530

    Add files via upload

---
+++ Conceptual_Programming-Practice/Day_08/ExceptionDemo2.py
@@ -0,0 +1,25 @@
+
+def main():
+    Ans = 0
+    try:
+        print("Inside try")
+
+        print("Enter first number :")
+        No1 = int(input())
+
+        print("Enter Second number :")
+        No2 = int(input())
+
+        Ans = No1 / No2
+    except:
+        print("Inside except")
+        ZeroDivisionError
+        ValueError
+    
+    finally:
+        print("Inside finalys")
+
+    print("Division is : ",Ans)
+
+if __name__ == "__main__":
+    main()
