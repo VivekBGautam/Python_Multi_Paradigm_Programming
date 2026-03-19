6a2d9b341df2607a415126bc9aa877bb7f80fe87
Vivek Gautam <vivekbgautam@gmail.com>
2026-03-19 22:50:10 +0530

    Add files via upload

---
+++ Conceptual_Programming-Practice/Day_09/ObjectOrientedX.py
@@ -0,0 +1,21 @@
+class Arithmatic:
+    def Addition(self,A,B):
+        return A + B
+
+    def Substraction(self,A,B):
+        return A - B
+
+No1 = 0
+No2 = 0
+And = 0
+
+No1 = int(input("Enter first number : "))
+No2 = int(input("Enter Second number : "))
+
+obj = Arithmatic()
+
+Ans = obj.Addition(No1,No2)
+print("Addition is :",Ans)
+
+Ans = obj.Substraction(No1,No2)
+print("Substraction is :",Ans)
