5bdb0c38abb8c16ad8ec0dcdb28c950ad0d08ef1
Vivek Gautam <vivekbgautam@gmail.com>
2026-03-19 22:47:24 +0530

    Add files via upload

---
+++ Conceptual_Programming-Practice/Day_09/BehavioursAll.py
@@ -0,0 +1,26 @@
+class Demo:
+    No = 10
+    
+    def __init__(self,A,B):
+        self.Value1 = A
+        self.Value2 = B
+
+    def Fun(self):
+        print("Inside Instance method fun : ",self.Value1,self.Value2)
+    
+    @classmethod
+    def Sun(cls):
+        print("Inside class method sun : ",cls.No)
+
+    @staticmethod
+    def Gun():
+        print("inside Static method Gun  : ",Demo.No)
+
+Demo.Sun()
+print("class variable No :",Demo.No)
+
+obj = Demo(11,21)
+obj.Fun()
+print("Instance variable :",obj.Value1,obj.Value2)
+
+Demo.Gun()
